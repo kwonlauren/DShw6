be0b292472c54b71fb4978e0e67fd63680f3bb20
kwonlauren <kwonlauren@snu.ac.kr>
2021-06-13 00:44:56 +0900

    graph에 추가

--- Graph.java
+++ Graph.java
@@ -3,6 +3,19 @@ import java.util.Hashtable;
 public class Graph {
     public Hashtable<String, htNode> ht;
 
+    public Graph(){
+        ht = new Hashtable<>();
+    }
+
+    public void addVertex(String stationNum, String name, String line){
+        ht.put(stationNum, new htNode(name, line));
+    }
+
+    public void addEdge(String startStation, String endStation, int time){
+        htNode node = ht.get(startStation);
+        node.adjacentStations.put(endStation, time);
+    }
+
 }
 
 class htNode{
--- Subway.java
+++ Subway.java
@@ -2,26 +2,36 @@ import java.io.*;
 
 public class Subway {
     public static void main(String[] args) throws Exception{
+        Graph map = new Graph();
         BufferedReader br = new BufferedReader(new FileReader(args[0]));
         String line;
-        while ((line = br.readLine()) != "\n") {
-            //vertex 추가
+        while (!(line = br.readLine()).isEmpty()) {
+            //graph에 vertex 추가
+            String[] strs = line.split(" ");
+            map.addVertex(strs[0], strs[1], strs[2]);
         }
         while ((line = br.readLine()) != null) {
-            //edge 추가
+            //graph에 edge 추가
+            String[] strs = line.split(" ");
+            map.addEdge(strs[0], strs[1], Integer.parseInt(strs[2]));
         }
+
 
         BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
         while (true)
         {
             String input = br2.readLine();
             if(input.compareTo("QUIT") == 0) break;
-            command(input);
+            String[] strs = input.split(" ");
+            // dijkstra(map, strs[0], strs[1]);
         }
+
     }
 
-    private static void command(String input)
+    private static String dijkstra(Graph G,String origin, String destination)
     {
+        //origin, destination은 역 이름(고유번호x)
+        //초기화
 
     }
 }
